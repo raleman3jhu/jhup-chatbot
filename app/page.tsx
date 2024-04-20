e812fcbb098800d6017456f619c25ef311e1f765
raleman3 <raleman3@jhu.edu>
2024-04-20 12:40:03 -0400

    style changes

--- app/page.tsx
+++ app/page.tsx
@@ -24,28 +24,28 @@ export default function Chat() {
   })
 
   return (
-    <div className='flex flex-row justify-center items-center h-screen border-red-500 border-solid border-2'>
+    <div className='flex flex-row justify-center items-center h-screen'>
       <Image
         src={coverImage}
         alt=''
         height={400}
         width={320}
         layout='fixed'
-        className='mx-6'
+        className='mx-6 hidden md:block'
       ></Image>
-      <div className='flex flex-col justify-between'>
+      <div className='flex flex-col justify-between rounded-md items-center max-h-full bg-slate-600 px-2 my-6 mx-2'>
         <div
           ref={scrollableChatRef}
           className='flex flex-col w-full max-w-lg max-h-80-screen mb-10 overflow-y-auto mx-auto stretch'
         >
-          <div className='rounded-md my-4 p-2 ml-2 mr-5 bg-slate-700'>
+          <div className='rounded-md text-lg my-4 p-2 ml-2 mr-5 bg-slate-700 border border-solid border-white'>
             Hi I&apos;m your virtual assistant. What would you like to know
             about the book &apos;Teaching with AI&apos;?
           </div>
           {messages.map((m, index) => (
             <div
               key={m.id}
-              className={`rounded-md text-xl my-4 p-2 whitespace-pre-wrap ${
+              className={`rounded-md text-lg my-4 p-2 whitespace-pre-wrap border border-solid border-white ${
                 index % 2 === 0
                   ? "ml-5 mr-2 bg-heritage-blue"
                   : "ml-2 mr-5 bg-slate-700"
@@ -56,13 +56,23 @@ export default function Chat() {
           ))}
           {loading && <p className='mx-auto animate-pulse'>Loading...</p>}
         </div>
-        <form onSubmit={handleSubmit}>
+        <form
+          onSubmit={handleSubmit}
+          className='w-full mb-4 flex justify-around'
+        >
           <input
-            className='fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black'
+            className='w-full max-w-lg p-2 border border-gray-300 rounded shadow-xl text-black'
             value={input}
-            placeholder='Say something...'
+            placeholder='What do you want to know?'
             onChange={handleInputChange}
           />
+          <button
+            type='submit'
+            aria-label='Submit question'
+            className='bg-heritage-blue py-2 px-4 mx-2 rounded-sm'
+          >
+            ↵
+          </button>
         </form>
       </div>
     </div>
